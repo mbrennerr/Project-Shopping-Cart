fccb8c5ac9cd00dd3c34de3bb98db5efb51eb1c1
mbrennerr <marciobrennerbusiness@gmail.com>
2021-08-04 14:52:09 -0300

    Requisito 1lint.

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-//GLOBALS VARIABLES;
+// GLOBALS VARIABLES;
 const itemSection = document.querySelector('.items');
-
-
-
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -22,7 +19,6 @@ function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
   itemSection.appendChild(section);
-
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
@@ -48,24 +44,17 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-const fetchApiList = (produto = "computador") => {
+const fetchApiList = (produto = 'computador') => {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${produto} `)
     .then((response) => {
       response.json().then(({ results }) => {
-
         results.forEach(({ id, title, thumbnail }) => {
-          createProductItemElement({ sku: id, name: title, image: thumbnail })
-
+          createProductItemElement({ sku: id, name: title, image: thumbnail });
         });
-
-
-      })
-    })
+      });
+    });
 };
 
-
-
 window.onload = () => {
-  fetchApiList()
-
+  fetchApiList();
 };
